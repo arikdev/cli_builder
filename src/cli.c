6d518d9f0a2c28d4c0601f99a18ad459dc0f48a7
Your Name <you@example.com>
2019-02-23 09:50:49 -0800

    Fixes.

--- src/cli.c
+++ src/cli.c
@@ -1,3 +1,25 @@
+/*
+Copyright (c) 2019 Arik Aelxander
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -21,27 +43,6 @@
 #define COLOR_GREEN "\033[32m"
 #define CLEAR_RIGHT "\033[K"
 #define COLOR_RESET "\033[0m"
-
-#define RULE "rule"
-#define TUPLE "tuple"
-#define FILENAME "file"
-#define PERM "perm"
-#define PROGRAM "program"
-#define USER "user"
-#define ACTION "action"
-#define SRC_IP "src_ip"
-#define SRC_NETMASK "src_netmask"
-#define DST_IP "dst_ip"
-#define DST_NETMASK "dst_netmask"
-#define IP_PROTO "proto"
-#define SRC_PORT "src_port"
-#define SDT_PORT "dst_port"
-#define CAN_MSG "can_mid"
-#define DIRECTION "direction"
-#define INTERFACE "interface"
-#define ACTION_OBJ "action_obj"
-#define ACTION "action"
-#define LOG "log"
 
 #define GET_NEXT_TOKEN(ptr, del) \
 	ptr = strtok(NULL, del); \
@@ -360,7 +361,7 @@ void handle_enter(char *buf)
 		words[count++] = p;
 	}
 	
-	if (count == 1) {
+	if (nodep1 && count == 1) {
                 // move to next level
                 cur_node = nodep;
                 cli_prompt[strlen(cli_prompt) - 1] = '/';
